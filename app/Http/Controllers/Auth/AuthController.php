d7c59e7ec61fa6f0c875dc4cdbfa846e6f7313d8
lvruizheng <15810312145@163.com>
2017-05-08 18:04:57 +0800

    first commit

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use Validator;
+use Crypt;
+use Config;
+use Cookie;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+
+class AuthController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Registration & Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users, as well as the
+    | authentication of existing users. By default, this controller uses
+    | a simple trait to add these behaviors. Why don't you explore it?
+    |
+    */
+
+    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+
+    /**
+     * Where to redirect users after login / registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+
+    /**
+     * Create a new authentication controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
+    
+    //登陆页面
+    public function getLogin(Request $request)
+    {
+        $arr = array('status'=>'200');
+        $reque = $request->all();
+        $rules    = [
+            'callback'     => 'required',
+            'details'      => 'required',
+            '_secret'      => 'required',
+        ];
+         
+        $v = Validator::make($reque, $rules);
+         
+        if ($v->fails()) {
+            $arr['status'] = 3;
+            return response($reque['callback'].'('.json_encode($arr).')');
+        }
+         
+        $user_cont = Crypt::decrypt($reque['details']);
+         
+        //验证是否有效
+        if($reque['_secret'] != MD5(array_push($user_cont,Config::get('constants.auth_secret')))){
+            $arr['status'] = 2;
+            return response($reque['callback'].'('.json_encode($arr).')');die;
+        }
+         
+        return response($reque['callback'].'('.json_encode($arr).')')->withCookie(Cookie::forever('nk', $user_cont['nk']))->withCookie(Cookie::forever('uid',$user_cont['uid']));
+    
+        //return view('auth.login');
+    }
+    
+    /**
+     * 登出
+     */
+    public function getLogout(Request $request)
+    {
+        $reque = $request->all();
+        $arr = array('status'=>'200');
+         
+        return response($reque['callback'].'('.json_encode($arr).')')->withCookie(Cookie::forget('nk'))->withCookie(Cookie::forget('uid'));
+        //return response('hello world')->withCookie(Cookie::forget('nk'))->withCookie(Cookie::forget('uid'));
+    }
+}
---
+++ bootstrap/app.php
@@ -0,0 +1,2 @@
+<?php
+return require_once LARAVEL_DIR.'/bootstrap/app.php';
