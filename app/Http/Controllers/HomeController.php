83c6b1622219fbbbfb15106443f3897d1ca46e3d
lvruizheng <15810312145@163.com>
2017-05-18 11:25:29 +0800

    5.2升级完成

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Services\Helper;
+use \Common\Common;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
@@ -21,8 +22,30 @@ class HomeController extends Controller
     
     public function index(Request $request)
     {
-//         $res = Helper::sendHttp();
+//         $res = Common::sendHttp('http://lrz.ucenter.com/api/router');
 // 	    var_dump($res);die;
+
+        
+        // 接口调试成功
+//         $client = new Client();
+        
+//         $form_params = array(
+//             'app_id' => '1',
+//             'method' => 'demo',
+//             'nonce' => 'abc',
+//         );
+        
+//         $sign = Common::generateMd5Sign($form_params);
+        
+//         $form_params['sign'] = $sign;
+        
+//         $res = $client->request('post','http://lrz.ucenter.com/api/router',[
+//             'form_params' => $form_params
+//         ]);
+        
+//         echo $res->getBody();die;
+         
+        
         return view('home');
     }
     
